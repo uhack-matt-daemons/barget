921835a1bce6d125c93fe5fe47410ad7286a6ae4
Josh Glendenning <joshglendenning@gmail.com>
2014-09-27 07:07:57 -0500

    Changed tab style

--- barget.rb
+++ barget.rb
@@ -3,39 +3,39 @@ require 'sinatra'
 Dir['src/*.rb'].each {|file| require File.expand_path file }
 
 configure do
-  set :public_folder, 'public'
-  enable :sessions
+	set :public_folder, 'public'
+	enable :sessions
 end
 
 get '/' do
-  'Hey, bitches. <a href="http://zxing.appspot.com/scan?ret=http%3A%2F%2F131.212.238.82:4567%2Fscanned%2F1%2F%7BCODE%7D&SCAN_FORMATS=UPC_A,EAN_13">click here fuckers</a>'
+	'Hey, bitches. <a href="http://zxing.appspot.com/scan?ret=http%3A%2F%2F131.212.238.82:4567%2Fscanned%2F1%2F%7BCODE%7D&SCAN_FORMATS=UPC_A,EAN_13">click here fuckers</a>'
 end
 
 get '/login' do
-  @msg = 'Bad login. Try again.' if params[:bad_attempt]
-  redirect '/profile' if session[:user_id]
-  erb File.read('views/login.erb')
+	@msg = 'Bad login. Try again.' if params[:bad_attempt]
+	redirect '/profile' if session[:user_id]
+	erb File.read('views/login.erb')
 end
 
 post '/login' do
-  user_id = $db.getUserID(params[:username])
-  if user_id
-    session[:user_id] = user_id
-    redirect '/login'
-  else
-    redirect '/login?bad_attempt=true'
-  end
+	user_id = $db.getUserID(params[:username])
+	if user_id
+		session[:user_id] = user_id
+		redirect '/login'
+	else
+		redirect '/login?bad_attempt=true'
+	end
 end
 
 get '/logout' do
-  session[:user_id] = nil
-  redirect '/login'
+	session[:user_id] = nil
+	redirect '/login'
 end
 
 get '/profile' do
-  @user = User.get(session[:user_id])
-  @items = @user.items.map {|i| Target.product(i)}
-  erb File.read('views/profile.erb')
+	@user = User.get(session[:user_id])
+	@items = @user.items.map {|i| Target.product(i)}
+	erb File.read('views/profile.erb')
 end
 
 get '/scanned/*/*' do |user,barcode|
@@ -43,14 +43,14 @@ get '/scanned/*/*' do |user,barcode|
 end
 
 get '/product/search/*' do |searchTerm|
-  view = ''
-  Target.product_search(searchTerm).each do |id|
-    view += Target.product(id).render
-  end
-  view
+	view = ''
+	Target.product_search(searchTerm).each do |id|
+		view += Target.product(id).render
+	end
+	view
 end
 
 get '/product/*' do |id|
-  product = Target.product(id)
-  product.render
+	product = Target.product(id)
+	product.render
 end
--- src/target_api.rb
+++ src/target_api.rb
@@ -1,52 +1,52 @@
 require 'httparty'
 
 class Target
-  include HTTParty
-  base_uri 'https://api.target.com/v2'
+	include HTTParty
+	base_uri 'https://api.target.com/v2'
 
-  @api_key = 'J5PsS2XGuqCnkdQq0Let6RSfvU7oyPwF'
-  @auth = {:key => @api_key}
+	@api_key = 'J5PsS2XGuqCnkdQq0Let6RSfvU7oyPwF'
+	@auth = {:key => @api_key}
 
-  def self.opts(query)
-    {
-        :headers => {'Accept' => 'application/json'},
-        :query => @auth.merge(query)
-    }
-  end
+	def self.opts(query)
+		{
+				:headers => {'Accept' => 'application/json'},
+				:query => @auth.merge(query)
+		}
+	end
 
-  def self.product_search(searchTerm, q = {})
-    q[:searchTerm] = searchTerm
-    results = self.get('/products/search', opts(q))
-    # returns an array of possible DPCI ids
-    results['CatalogEntryView'].map {|e| e['DPCI']}
-  end
+	def self.product_search(searchTerm, q = {})
+		q[:searchTerm] = searchTerm
+		results = self.get('/products/search', opts(q))
+		# returns an array of possible DPCI ids
+		results['CatalogEntryView'].map {|e| e['DPCI']}
+	end
 
-  def self.product(id, q = {'idType' => 'DPCI'})
-    Product.new(self.get("/products/#{id}", opts(q)))
-  end
+	def self.product(id, q = {'idType' => 'DPCI'})
+		Product.new(self.get("/products/#{id}", opts(q)))
+	end
 
 end
 
 class Product
-  def initialize(data)
-    @data = data['CatalogEntryView'][0]
-  end
+	def initialize(data)
+		@data = data['CatalogEntryView'][0]
+	end
 
-  def title
-    @data['title']
-  end
+	def title
+		@data['title']
+	end
 
-  def price
-    @data['Offers'][0]['OfferPrice'][0]['formattedPriceValue']
-  end
+	def price
+		@data['Offers'][0]['OfferPrice'][0]['formattedPriceValue']
+	end
 
-  def image_url
-    @data['Images'][0]['PrimaryImage'][0]['image']
-  end
+	def image_url
+		@data['Images'][0]['PrimaryImage'][0]['image']
+	end
 
-  def render
-    require 'erubis'
-    view = Erubis::Eruby.new(File.read(File.expand_path 'views/product.erb'))
-    view.result(:item => self)
-  end
+	def render
+		require 'erubis'
+		view = Erubis::Eruby.new(File.read(File.expand_path 'views/product.erb'))
+		view.result(:item => self)
+	end
 end
